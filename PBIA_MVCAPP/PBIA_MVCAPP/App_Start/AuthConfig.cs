8a2de0bbaf29ed2a15128007406745012d6f601d
Jerzy Goworski <jgoworski@wi.ps.pl>
2015-05-31 14:24:08 +0200

    Added .NET MVC 4  Project (our website) and WebService

---
+++ PBIA_MVCAPP/PBIA_MVCAPP/App_Start/AuthConfig.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using PBIA_MVCAPP.Models;
+using WebMatrix.WebData;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity;
+using System.Threading;
+
+namespace PBIA_MVCAPP
+{
+    public static class AuthConfig
+    {
+        private static SimpleMembershipInitializer _initializer;
+        private static object _initializerLock = new object();
+        private static bool _isInitialized;
+
+        public static void RegisterAuth()
+        {
+            LazyInitializer.EnsureInitialized(ref _initializer, ref _isInitialized, ref _initializerLock);
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            //OAuthWebSecurity.RegisterFacebookClient(
+            //    appId: "",
+            //    appSecret: "");
+
+            //OAuthWebSecurity.RegisterGoogleClient();
+            
+        }
+    }
+
+    class SimpleMembershipInitializer
+    {
+        public SimpleMembershipInitializer()
+        {
+            Database.SetInitializer<UsersContext>(null);
+
+            try
+            {
+                using (var context = new UsersContext())
+                {
+                    if (!context.Database.Exists())
+                    {
+                        // Create the SimpleMembership database without Entity Framework migration schema
+                        ((IObjectContextAdapter)context).ObjectContext.CreateDatabase();
+                    }
+                }
+
+                WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("The ASP.NET Simple Membership database could not be initialized. For more information, please see http://go.microsoft.com/fwlink/?LinkId=256588", ex);
+            }
+        }
+    }
+}
