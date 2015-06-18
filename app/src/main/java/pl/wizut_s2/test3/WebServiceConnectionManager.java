5479941b7e773dc8d9fcf9932c18a4c69ed85e3b
KrzaQ <github@krzaq.cc>
2015-06-18 19:38:22 +0200

    Accepting any SSL certificate. Security FTW!

--- app/src/main/java/pl/wizut_s2/test3/WebServiceConnectionManager.java
+++ app/src/main/java/pl/wizut_s2/test3/WebServiceConnectionManager.java
@@ -12,6 +12,15 @@ import org.ksoap2.transport.HttpTransportSE;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
 
 public class WebServiceConnectionManager {
 
@@ -26,6 +35,8 @@ public class WebServiceConnectionManager {
 
     WebServiceConnectionManager(MainActivity ma) {
         mainActivity = ma;
+
+        allowAllSSL();
 
         AsyncTaskRunner runner = new AsyncTaskRunner();
         runner.execute();
@@ -100,4 +111,59 @@ public class WebServiceConnectionManager {
             mainActivity.displayNotification(text[0]);
         }
     }
+
+    private static TrustManager[] trustManagers;
+
+    public static class _FakeX509TrustManager implements
+            javax.net.ssl.X509TrustManager {
+        private static final X509Certificate[] _AcceptedIssuers = new X509Certificate[]{};
+
+        public void checkClientTrusted(X509Certificate[] arg0, String arg1)
+                throws CertificateException {
+        }
+
+        public void checkServerTrusted(X509Certificate[] arg0, String arg1)
+                throws CertificateException {
+        }
+
+        public boolean isClientTrusted(X509Certificate[] chain) {
+            return (true);
+        }
+
+        public boolean isServerTrusted(X509Certificate[] chain) {
+            return (true);
+        }
+
+        public X509Certificate[] getAcceptedIssuers() {
+            return (_AcceptedIssuers);
+        }
+    }
+
+    public static void allowAllSSL() {
+
+        javax.net.ssl.HttpsURLConnection
+                .setDefaultHostnameVerifier(new HostnameVerifier() {
+                    public boolean verify(String hostname, SSLSession session) {
+                        return true;
+                    }
+                });
+
+        javax.net.ssl.SSLContext context = null;
+
+        if (trustManagers == null) {
+            trustManagers = new javax.net.ssl.TrustManager[]{new _FakeX509TrustManager()};
+        }
+
+        try {
+            context = javax.net.ssl.SSLContext.getInstance("TLS");
+            context.init(null, trustManagers, new SecureRandom());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("allowAllSSL", e.toString());
+        } catch (KeyManagementException e) {
+            Log.e("allowAllSSL", e.toString());
+        }
+        javax.net.ssl.HttpsURLConnection.setDefaultSSLSocketFactory(context
+                .getSocketFactory());
+    }
+
 }
